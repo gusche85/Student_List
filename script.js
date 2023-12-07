8e7fdf49d81b39494271f79c24cc10249c037565
gusche85 <149746619+gusche85@users.noreply.github.com>
2023-12-07 16:15:26 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const students =[];
+
+function addStudent() {
+  const name = prompt("Enter the student's name:");
+  const grade = parseInt(prompt("Enter the student's grade:"));
+  const studentClass = prompt("Enter the student's class:");
+  const student = {
+    name: name,
+    grade: grade,
+    class: studentClass
+  };
+  students.push(student);
+  alert('Student added!')
+}
+
+function viewStudents() {
+  let studentList = "List of students:\n";
+  for (const student of students) {
+    studentList += `${student.name} from class ${student.class}: ${student.grade}\n`;
+  }
+  alert(studentList);
+}
